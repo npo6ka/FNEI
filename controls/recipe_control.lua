176f51d4cd8f4ffb18dd244aa410806017973b32
npo6ka <npo6ka100@gmail.com>
2017-06-09 22:29:06 +0300

    add save previous recipe page

--- controls/recipe_control.lua
+++ controls/recipe_control.lua
@@ -53,7 +53,7 @@ function fnei.rc.open_gui(player)
   local cur_page = fnei.rc.recipe_page
   local recipe = fnei.rc.recipe_list[cur_page]
   fnei.gui.open_recipe_gui(player, recipe, cur_page, fnei.rc.get_recipe_amount())
-  end
+end
 
 function fnei.rc.set_new_recipe_list(player)
   local elem_num = #fnei.rc.search_stack
@@ -62,8 +62,8 @@ function fnei.rc.set_new_recipe_list(player)
   end
   local element = fnei.rc.search_stack[elem_num]
 
-  if (element.type == "craft" and not fnei.rc.set_recipe_list(get_craft_recipe_list(player, element.name))) or
-     (element.type == "usage" and not fnei.rc.set_recipe_list(get_usage_recipe_list(player, element.name))) then
+  if (element.type == "craft" and not fnei.rc.set_recipe_list(get_craft_recipe_list(player, element.name), element.page)) or
+     (element.type == "usage" and not fnei.rc.set_recipe_list(get_usage_recipe_list(player, element.name), element.page)) then
     return
   end
   fnei.rc.open_gui(player)
@@ -80,7 +80,7 @@ function fnei.rc.insert_recipe_name(player, element_name, element_type)
           prev_elem = fnei.rc.search_stack[#fnei.rc.search_stack]
         end
         if prev_elem == nil or prev_elem.name ~= element_name or prev_elem.type ~= element_type then 
-        table.insert(fnei.rc.search_stack, {name = element_name, type = element_type})
+        table.insert(fnei.rc.search_stack, {name = element_name, type = element_type, page = 1})
         return true
       end
     end
@@ -88,16 +88,23 @@ function fnei.rc.insert_recipe_name(player, element_name, element_type)
   return false
 end
 
+function fnei.rc.set_cur_recipe_page()
+  local cur_recipe = fnei.rc.search_stack[#fnei.rc.search_stack]
+  if cur_recipe then 
+    cur_recipe.page = fnei.rc.recipe_page
+  end
+end
+
 function fnei.rc.delete_recipe_name()
   table.remove(fnei.rc.search_stack)
 end
 -----------------------Recipe_List-------------------------
-function fnei.rc.set_recipe_list(list)
+function fnei.rc.set_recipe_list(list, page)
   if not list or #list == 0 then
     return false
   end
   fnei.rc.recipe_list = list
-  fnei.rc.set_recipe_page(1)
+  fnei.rc.set_recipe_page(page)
   return true
 end
 
@@ -119,12 +126,10 @@ function fnei.rc.set_recipe_page(value)
   local max_page =  fnei.rc.get_recipe_amount()
   if value < 1 then 
     fnei.rc.recipe_page = max_page
-    return
-  end
- 
-  if value > max_page then
+  elseif value > max_page then
     fnei.rc.recipe_page = 1
-    return
+  else
+    fnei.rc.recipe_page = value
   end
-  fnei.rc.recipe_page = value
+  fnei.rc.set_cur_recipe_page()
 end
