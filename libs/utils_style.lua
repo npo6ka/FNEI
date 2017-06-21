e09f7469b98e18d44b3121fda734baef6dddb1b1
terreo <terreo@users.noreply.github.com>
2017-06-21 04:59:51 -0700

    Update utils_style.lua
    Another mod had added items without "hidden" flag. This caused a crash when populating the search gui.

--- libs/utils_style.lua
+++ libs/utils_style.lua
@@ -17,7 +17,7 @@ end
 
 function set_style(element)
   local item = game.item_prototypes[element.name]
-  if item and item.flags["hidden"] == true then
+  if item and item.has_flag("hidden") and item.flags["hidden"] == true then
     element.style = "red_slot_button_style"
   else
     element.style = "slot_button_style"
