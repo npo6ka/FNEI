ad56efdf5b53b186a7f79b779eb0736bf530f742
npo6ka <npo6ka100@gmail.com>
2017-12-30 01:57:19 +0300

    add more gui element and add a few setting

--- unsort/gui.lua
+++ unsort/gui.lua
@@ -13,18 +13,22 @@ function Gui.create_gui_name(cont_name, gui_name)
   return Gui.get_prefix() .. '_' .. cont_name .. '_' .. gui_name
 end
 
-
-function Gui.addSpriteButton(parent, cont_name, gui_name, tooltip, style, event_handler)
+function set_def_fields(gui_type, cont_name, gui_name, style)
   local gui_elem = {
     type = "sprite-button",
     name = Gui.create_gui_name(cont_name, gui_name),
   }
 
-  if style then
-    gui_elem.style = style
-  else
+  if style == "" then
     gui_elem.style = gui_elem.name
+  else
+    gui_elem.style = style
   end
+  return gui_elem
+end
+
+function Gui.addSpriteButton(parent, cont_name, gui_name, tooltip, style, event_handler)
+  local gui_elem = set_def_fields("sprite-button", cont_name, gui_name, style)
 
   if tooltip then
     gui_elem.tooltip = tooltip
@@ -35,6 +39,35 @@ function Gui.addSpriteButton(parent, cont_name, gui_name, tooltip, style, event_
   parent.add(gui_elem)
 
   Events.add_custom_event(cont_name, gui_elem.type, gui_name, event_handler)
+end
+
+function Gui.addFlow(parent, cont_name, gui_name, style)
+  local gui_elem = set_def_fields("flow", cont_name, gui_name, style)
+  parent.add(gui_elem)
+end
+
+function Gui.addFrame(parent, cont_name, gui_name, style)
+  local gui_elem = set_def_fields("frame", cont_name, gui_name, style)
+  parent.add(gui_elem)
+end
+
+function Gui.addTable(parent, cont_name, gui_name, style, colspan)
+  local gui_elem = set_def_fields("table", cont_name, gui_name, style)
+  gui_elem.colspan = colspan
+  parent.add(gui_elem)
+end
+
+function Gui.addDropDown(parent, cont_name, gui_name, style, items, selected_index)
+  local gui_elem = set_def_fields("drop-down", cont_name, gui_name, style)
+  gui_elem.items = items
+  gui_elem.selected_index = selected_index
+  parent.add(gui_elem)
+end
+
+function Gui.addCheckbox(parent, cont_name, gui_name, style, state)
+  local gui_elem = set_def_fields("checkbox", cont_name, gui_name, style)
+  gui_elem.state = state
+  parent.add(gui_elem)
 end
 
 return Gui
--- unsort/settings.lua
+++ unsort/settings.lua
@@ -1,7 +1,21 @@
-local  = {
-  classname = "FN"
+local Settings = {
+  classname = "FNSettings"
 }
 
+local settings_list = {
+  {type = "checkbox", name = "option-1"},
+}
 
+function Settings.get_list() 
+  return settings_list
+end
 
-return 
+function Settings.get(sett_name) 
+  
+end
+
+function Settings.set(sett_name, val) 
+  
+end
+
+return Settings
