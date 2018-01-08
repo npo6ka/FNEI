e48a6e8edcbf8561aee66b5f25a34f0f37f8bc80
npo6ka <npo6ka100@gmail.com>
2018-01-09 00:44:39 +0300

    fix crash problem

--- libs/utils_style.lua
+++ libs/utils_style.lua
@@ -1,7 +1,7 @@
 function set_style(element)
   local item = game.item_prototypes[element.name]
   if item and item.has_flag("hidden") and item.flags["hidden"] == true then
-    element.style = "red_slot_button_style"
+    element.style = "fnei_red_building_button_style"
   else
     element.style = "slot_button"
   end
