1d23b8a2be82f2bf78151bca070ccfde1d0cf143
npo6ka <npo6ka100@gmail.com>
2018-02-24 15:50:00 +0300

    add saving last viewed research

--- unsort/recipe_controller.lua
+++ unsort/recipe_controller.lua
@@ -30,14 +30,8 @@ function RecipeController.open()
   return gui
 end
 
-function RecipeController.open_new_recipes()
-  RecipeController.set_page_list()
-  RecipeController.change_page_event()
-end
-
 function RecipeController.back_key()
   queue:remove()
-  pages:set_cur_page(1)
   RecipeController.open_new_recipes()
   return queue:is_empty()
 end
@@ -49,6 +43,138 @@ end
 function RecipeController.get_name()
   return RecipeGui.name
 end
+
+----------------------------------- gui ---------------------------------------
+
+function RecipeController.open_new_recipes()
+  RecipeController.set_page_list()
+  RecipeController.change_page_event()
+end
+
+function RecipeController.draw_recipe()
+  local recipe = pages:get_list_for_tab(pages:get_cur_page())
+
+  if recipe and recipe[1] then
+    recipe = recipe[1]
+  else
+    return
+  end
+
+  RecipeGui.set_recipe_name(recipe)
+  RecipeGui.set_recipe_icon(recipe)
+  RecipeGui.set_ingredients(recipe.ingredients)
+  RecipeGui.set_recipe_time(recipe.energy)
+  RecipeGui.set_products(recipe.products)
+  RecipeGui.set_made_in_list(recipe)
+  RecipeGui.set_techs(recipe)
+end
+
+function RecipeController.draw_paging()
+  RecipeGui.draw_paging(pages)
+end
+
+function RecipeController.set_crafting_type()
+  local cur_prot = queue.get() or {}
+  RecipeGui.set_crafting_type(cur_prot.action_type)
+end
+
+function RecipeController.draw_cur_prot()
+  local cur_prot = queue.get() or {}
+  RecipeGui.draw_cur_prot(cur_prot.type, cur_prot.name)
+end
+
+----------------------------------- queue ---------------------------------------
+
+function RecipeController.add_element_in_new_queue(action_type, prot_type, prot_name)
+  queue:clear()
+  RecipeController.add_element(action_type, prot_type, prot_name)
+end
+
+function RecipeController.add_element(action_type, prot_type, prot_name)
+  local last_elem = queue:get()
+
+  if last_elem == nil or (prot_name ~= last_elem.name or prot_type ~= last_elem.type or action_type ~= last_elem.action_type) then
+    local recipe_list = RecipeController.get_recipe_list(action_type, prot_type, prot_name)
+
+    if recipe_list and #recipe_list > 0 then
+      queue:add({ type = prot_type, name = prot_name, action_type = action_type, page = RecipeController.get_page_name_for_recipe(action_type, prot_type, prot_name) })
+    end
+  end
+end
+
+function RecipeController.get_page_name_for_recipe(action_type, prot_type, prot_name)
+  local global = Player.get_global()
+
+  if not global.recipe_page then global.recipe_page = {} end
+
+  if action_type and prot_type and prot_name then
+    local name = action_type .. prot_type .. prot_name
+
+    return global.recipe_page[name]
+  else
+    Debug:error("Error in function RecipeController.get_page_name_for_recipe: nil value")
+  end
+end
+
+function RecipeController.set_page_name_for_recipe(action_type, prot_type, prot_name, val)
+  local global = Player.get_global()
+
+  if not global.recipe_page then global.recipe_page = {} end
+
+  if action_type and prot_type and prot_name then
+    local name = action_type .. prot_type .. prot_name
+
+    global.recipe_page[name] = val
+  else
+    Debug:error("Error in function RecipeController.set_page_name_for_recipe: nil value")
+  end
+end
+
+function RecipeController.save_page()
+  local val = queue:get()
+
+  if val then
+    local list = pages:get_list_for_tab(pages:get_cur_page())
+
+    if list and list[1] then
+      RecipeController.set_page_name_for_recipe(val.action_type, val.type, val.name, list[1])
+      val.page = list[1]
+out("test paging:", queue:get().page, "")
+    end
+  end
+end
+
+----------------------------------- paging ---------------------------------------
+
+function RecipeController.set_page_list()
+  local last_prot = queue.get()
+
+  if last_prot then
+    pages:set_page_list(RecipeController.get_recipe_list(last_prot.action_type, last_prot.type, last_prot.name))
+  end
+
+  RecipeController.set_cur_page()
+end
+
+function RecipeController.set_cur_page()
+  local val = queue.get()
+
+  if val and val.page then
+    local end_val = pages:amount_page()
+    
+    for i = 1,end_val do
+      local recipe = pages:get_list_for_tab(i)
+      if recipe and recipe[1] == val.page then
+        pages:set_cur_page(i)
+        return
+      end
+    end
+  end
+
+  pages:set_cur_page(1)
+end
+
+-------------------------------- events ----------------------------------------
 
 function RecipeController.open_item_recipe_event(event, elem_name)
   if elem_name == "item" then
@@ -96,80 +222,15 @@ function RecipeController.open_tech_event(event, elem_name, split_names)
   end
 end
 
---------------------------------------------------------------------------
-
-
-function RecipeController.add_element_in_new_queue(action_type, prot_type, prot_name)
-  queue:clear()
-  RecipeController.add_element(action_type, prot_type, prot_name)
-end
-
-function RecipeController.add_element(action_type, prot_type, prot_name)
-  local last_elem = queue:get()
-
-  if last_elem == nil or (prot_name ~= last_elem.name or prot_type ~= last_elem.type or action_type ~= last_elem.action_type) then
-    local recipe_list = RecipeController.get_recipe_list(action_type, prot_type, prot_name)
-
-    if recipe_list and #recipe_list > 0 then
-      queue:add({ type = prot_type, name = prot_name, action_type = action_type })
-      pages:set_cur_page(1)
-    end
-  end
-end
-
-function RecipeController.set_page_list()
-  local last_prot = queue.get()
-
-  if last_prot then
-    pages:set_page_list(RecipeController.get_recipe_list(last_prot.action_type, last_prot.type, last_prot.name))
-  end
-end
-
-function RecipeController.draw_recipe()
-  local recipe = pages:get_list_for_tab(pages:get_cur_page())
-
-  if recipe and recipe[1] then
-    recipe = recipe[1]
-  else
-    return
-  end
-
-  RecipeGui.set_recipe_name(recipe)
-  RecipeGui.set_recipe_icon(recipe)
-  RecipeGui.set_ingredients(recipe.ingredients)
-  RecipeGui.set_recipe_time(recipe.energy)
-  RecipeGui.set_products(recipe.products)
-  RecipeGui.set_made_in_list(recipe)
-  RecipeGui.set_techs(recipe)
-end
-
-function RecipeController.draw_paging()
-  RecipeGui.draw_paging(pages)
-end
-
-function RecipeController.set_crafting_type()
-  local cur_prot = queue.get() or {}
-  RecipeGui.set_crafting_type(cur_prot.action_type)
-end
-
-function RecipeController.draw_cur_prot()
-  local cur_prot = queue.get() or {}
-  RecipeGui.draw_cur_prot(cur_prot.type, cur_prot.name)
-end
-
-
-function RecipeController.add_prot_event(event, name)
-  -- body
-end
-
 function RecipeController.change_page_event()
   RecipeController.draw_paging()
   RecipeController.draw_recipe()
   RecipeController.set_crafting_type()
   RecipeController.draw_cur_prot()
+  RecipeController.save_page()
 end
 
---------------------------------------------------------------------------------------
+-------------------------------- recipe list ----------------------------------------
 
 function RecipeController.get_recipe_list(action_type, type, prot)
   local recipe_list = {}
